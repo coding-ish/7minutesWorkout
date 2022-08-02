6c736dc00c5605d8f9f7abc6980579e555e6e2f8
Ishmael A. Mendez <ishmael.mendez@us.af.mil>
2022-08-02 13:51:58 -0500

    added toolbar, created binding for bmi activity

--- app/src/main/java/com/example/a7minutesworkout/BMI.kt
+++ app/src/main/java/com/example/a7minutesworkout/BMI.kt
@@ -2,10 +2,15 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a7minutesworkout.databinding.ActivityBmiactivityBinding
 
 class BMIActivity : AppCompatActivity() {
+
+    private var binding : ActivityBmiactivityBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bmiactivity)
+        binding = ActivityBmiactivityBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
     }
 }
