e3d5efe43f1a2c4af71694f6ccf2fd3abc918dd5
Ishmael A. Mendez <ishmael.mendez@us.af.mil>
2022-08-02 08:55:53 -0500

    updated values

--- app/src/main/java/com/example/a7minutesworkout/FinishScreen.kt
+++ app/src/main/java/com/example/a7minutesworkout/FinishScreen.kt
@@ -2,10 +2,21 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.a7minutesworkout.databinding.ActivityFinishScreenBinding
 
 class FinishScreen : AppCompatActivity() {
+    private var binding: ActivityFinishScreenBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_finish_screen)
+        binding = ActivityFinishScreenBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarFinishActivity)
+
+        if(supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
     }
 }
