83e343add89a13bfd4a8529fae42511fb7a71179
Ishmael A. Mendez <ishmael.mendez@us.af.mil>
2022-08-02 15:30:18 -0500

    updated values

--- app/src/main/java/com/example/a7minutesworkout/BMI.kt
+++ app/src/main/java/com/example/a7minutesworkout/BMI.kt
@@ -2,6 +2,7 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.a7minutesworkout.databinding.ActivityBmiactivityBinding
 
 class BMIActivity : AppCompatActivity() {
@@ -24,5 +25,45 @@ class BMIActivity : AppCompatActivity() {
 
             onBackPressed()
         }
+
+        binding?.btnCalculate?.setOnClickListener {
+            if(validateMetricUnits()){
+                val heightValue: Float = binding?.etMetricUnitHeight?.text.toString().toFloat()/100
+
+                val weightValue: Float = binding?.etMetricUnitWeight?.text.toString().toFloat()
+
+                val bmi = weightValue/(heightValue * heightValue)
+            }
+        }
+    }
+
+    private fun displayBMIResult(bmi: Float){
+
+        val bmiLabel: String
+        val bmiDescription:String
+
+        if(bmi.compareTo(15f) <= 0){
+            bmiLabel = "very severely underweight"
+            bmiDescription = "Oops! You need to eat more to be healthy!"
+        }else if(bmi.compareTo(15f) > 0 && bmi.compareTo(16f)<=0){
+         bmiLabel = "severely underweight"
+         bmiDescription = "Oops! You need to eat more to be healthy!"
+        }
+        binding?.llDisplayBMIResult?.visibility = View.VISIBLE
+        binding?.tvBMIResult?.text = bmi.toString()
+
+    }
+
+    private fun validateMetricUnits(): Boolean{
+
+        var isValid = true
+
+        if(binding?.etMetricUnitWeight?.text.toString().isEmpty()){
+            isValid = false
+        } else if(binding?.etMetricUnitHeight?.text.toString().isEmpty()){
+            isValid = false
+        }
+
+        return isValid
     }
 }
